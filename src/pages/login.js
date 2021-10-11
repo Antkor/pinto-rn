4ab8ef1d42e940adee182c88ff412a2a1ec6001c
antkor <antkorsc@gmail.com>
2021-10-11 09:22:50 +0300

    styled login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -16,29 +16,48 @@ export default function newsDetails({ navigation }) {
         resizeMode="contain"
       />
       <View style={styles.inputContainer}>
-        <Ionicons name="at" size={20}></Ionicons>
-        <Text>Email</Text>
-        <TextInput onChangeText={(text) => setEmail(text)}></TextInput>
+        <View style={styles.inputLabel}>
+          <Ionicons name="at" size={20}></Ionicons>
+          <Text>Email</Text>
+        </View>
+        <TextInput
+          style={styles.inputField}
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
       </View>
       <View style={styles.inputContainer}>
-        <Ionicons name="key-outline" size={20}></Ionicons>
-        <Text>password</Text>
+        <View style={styles.inputLabel}>
+          <Ionicons name="key-outline" size={20}></Ionicons>
+          <Text>Password</Text>
+        </View>
         <TextInput
+          style={styles.inputField}
           onChangeText={(text) => setEmail(text)}
           secureTextEntry={true}
         ></TextInput>
       </View>
-      <View style={styles.inputContainer}>
+      <View style={styles.buttonContainer}>
         <FontAwesome.Button
+          style={styles.primaryButton}
           onPress={() => {
             signIn({ userName: "1", password: "1" });
           }}
+          borderRadius={0}
+          backgroundColor="#75C8CC"
+          color="#fff"
         >
           Login
         </FontAwesome.Button>
-        <FontAwesome.Button>Register</FontAwesome.Button>
+        <FontAwesome.Button
+          borderRadius={0}
+          style={styles.secondaryButton}
+          backgroundColor="#fff"
+          color="#75C8CC"
+        >
+          Register
+        </FontAwesome.Button>
       </View>
-      <Text>Forgot your password?</Text>
+      <Text style={styles.forgotPassword}>Forgot your password?</Text>
     </View>
   );
 }
@@ -53,9 +72,43 @@ const styles = StyleSheet.create({
   logo: {
     width: 140,
     height: 60,
+    marginBottom: 30,
   },
   inputContainer: {
     display: "flex",
+    width: "80%",
+  },
+  inputLabel: {
     flexDirection: "row",
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  inputField: {
+    borderColor: "#75C8CC",
+    borderWidth: 2,
+    height: 40,
+  },
+  buttonContainer: {
+    display: "flex",
+    width: "80%",
+    flexDirection: "row",
+    marginTop: 20,
+    marginBottom: 20,
+    justifyContent: "space-between"
+  },
+  primaryButton: {
+    justifyContent: "center",
+    width: 145,
+  },
+  secondaryButton: {
+    borderWidth: 2,
+    borderColor: "#75C8CC",
+    justifyContent: "center",
+    width: 145
+  },
+  forgotPassword: {
+    color: "#DF9882",
+    fontSize: 16,
+    marginBottom: 40
   },
 });
